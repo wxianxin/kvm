c8fde61a9d502a4b8e3c011f32205a38af06d755
wxianxin <wxianxinreg@gmail.com>
2018-09-30 12:21:42 -0400

    update

--- vm.sh
+++ vm.sh
@@ -3,13 +3,12 @@ export VGAPT_FIRMWARE_BIN=/usr/share/OVMF/OVMF_CODE.fd
 export VGAPT_FIRMWARE_VARS=/usr/share/OVMF/OVMF_VARS.fd
 export VGAPT_FIRMWARE_VARS_TMP=/tmp/OVMF_VARS.fd.tmp
 
-
 cp -f $VGAPT_FIRMWARE_VARS $VGAPT_FIRMWARE_VARS_TMP &&
 qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly,file=$VGAPT_FIRMWARE_BIN \
   -drive if=pflash,format=raw,file=$VGAPT_FIRMWARE_VARS_TMP \
   -enable-kvm \
-  -machine q35,accel=kvm,mem-merge=on \
+  -machine q35,accel=kvm,mem-merge=off \
   -cpu host,kvm=off,hv_vendor_id=vgaptrocks,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
   -smp 10,sockets=1,cores=5,threads=2 \
   -m 10240 \
@@ -24,9 +23,7 @@ qemu-system-x86_64 \
 ;
 
 
-# -drive file=/home/xxxx/Win10_1803_English_x64.iso,media=cdrom \
-# -drive file=/virtio-win-0.1.149.iso,media=cdrom \
+# -drive file=/home/coupe/Win10_1803_English_x64.iso,media=cdrom \
+# -drive file=/home/coupe/virtio-win-0.1.160.iso,media=cdrom \
 # -net nic,model=virtio \
 
-# -device virtio-scsi-pci,id=scsi \
-# -serial none -parallel none \
