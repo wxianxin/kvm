191a3d3ef0b8d204d67e0524040420bc8e2b5784
wxianxin <wxianxinreg@gmail.com>
2020-06-12 11:51:01 -0400

    update for AMD and Fedora

--- vm.sh
+++ vm.sh
@@ -20,21 +20,21 @@ qemu-system-x86_64 \
   -m 10240 \
   -vga none \
   -rtc base=localtime \
-  -device vfio-pci,host=01:00.0,multifunction=on,romfile=/home/coupe/D/vm/TU106.rom \
-  -device vfio-pci,host=01:00.1 \
-  -device vfio-pci,host=01:00.2 \
-  -device vfio-pci,host=01:00.3 \
+  -boot menu=on \
+  -device vfio-pci,host=03:00.0,romfile=/home/coupe/D/vm/navi_10.rom \
+  -device vfio-pci,host=03:00.1 \
   -drive file=/home/coupe/D/vm/kvm_win10.qcow2,format=qcow2,if=virtio,cache=none,index=0 \
-  -usb -device usb-host,hostbus=1,hostaddr=5 \
-  -usb -device usb-host,hostbus=1,hostaddr=8 \
+  -drive file=/dev/nvme0n1p4,format=raw,cache=none,index=2 \
+  -drive file=/home/coupe/D/vm/virtio-win-0.1.171.iso,media=cdrom \
+  -usb -device usb-host,hostbus=1,hostaddr=2 \
+  -usb -device usb-host,hostbus=3,hostaddr=2 \
 ;
 
 
 # -vga none \
 # -device vfio-pci,host=01:00.0,romfile=/home/coupe/D/vm/TU106.rom \
-# -drive file=/dev/sda,format=raw,if=virtio,cache=none,index=0 \
+# -drive file=/dev/sda,format=raw,if=virtio,cache=none,index=1 \
 # -drive file=/home/coupe/D/vm/kvm_win10.qcow2,format=qcow2,if=virtio,cache=none,index=0 \
-# -drive file=/home/coupe/D/vm/Win10_1909_English_x64.iso,media=cdrom \
+# -drive file=/home/coupe/D/vm/Win10_2004_English_x64.iso,media=cdrom \
 # -drive file=/home/coupe/D/vm/virtio-win-0.1.171.iso,media=cdrom \
 # -net nic,model=virtio \
-
