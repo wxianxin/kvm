d1cfd197320fb8d9172cef11c9f70dd1adc979f1
wxianxin <wxianxinreg@gmail.com>
2020-06-26 11:44:09 -0400

    update

--- vm.sh
+++ vm.sh
@@ -5,6 +5,13 @@
 # sudo sh -c 'echo "0000:01:00.2" > /sys/bus/pci/drivers/vfio-pci/bind'
 
 # Standard locations from the Ubuntu `ovmf` package; last one is arbitrary:
+
+bash /home/coupe/kvm/bind_vfio.sh
+bash /home/coupe/kvm/set_cpu_performance.sh
+
+mount -t hugetlbfs hugetlbfs /dev/hugepages
+sysctl vm.nr_hugepages=8200
+
 export VGAPT_FIRMWARE_BIN=/usr/share/OVMF/OVMF_CODE.fd
 export VGAPT_FIRMWARE_VARS=/usr/share/OVMF/OVMF_VARS.fd
 export VGAPT_FIRMWARE_VARS_TMP=/tmp/OVMF_VARS.fd.tmp
@@ -15,7 +22,7 @@ taskset 0xFFF0 qemu-system-x86_64 \
   -drive if=pflash,format=raw,file=$VGAPT_FIRMWARE_VARS_TMP \
   -enable-kvm \
   -machine q35,accel=kvm,mem-merge=off \
-  -cpu host,kvm=off,hv_vendor_id=vgaptrocks,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
+  -cpu host,kvm=off,topoext=on,hv_relaxed,hv_vapic,hv_time,hv_vpindex,hv_synic,hv_vendor_id=1234567890ab,hv_spinlocks=0x1fff \
   -smp 12,sockets=1,cores=6,threads=2 \
   -m 16384 \
   -mem-prealloc \
@@ -35,8 +42,10 @@ taskset 0xFFF0 qemu-system-x86_64 \
   -usb -device usb-host,hostbus=5,hostaddr=3 \
 ;
 
+bash /home/coupe/kvm/set_cpu_ondemand.sh
 
 # taskset 0xFFF0 qemu-system-x86_64 \
+# -cpu host,kvm=off,topoext=on,hv_relaxed,hv_vapic,hv_time,hv_vpindex,hv_synic,hv_vendor_id=1234567890ab,hv_spinlocks=0x1fff \
 # -m 16384 \
 # -m 16384 -mem-prealloc -mem-path /dev/hugepages \
 # -vga none \
@@ -47,3 +56,5 @@ taskset 0xFFF0 qemu-system-x86_64 \
 # -drive file=/home/coupe/D/vm/virtio-win-0.1.171.iso,media=cdrom \
 # -device vfio-pci,host=01:00.0,romfile=/home/coupe/D/vm/navi_10.rom \
 # -net nic,model=virtio \
+
+# hv_vendor_id is used for Nvidia Error 43 prevention
