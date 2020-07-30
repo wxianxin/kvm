c6bdccf81229548565f9ca9b67b66fc21a7153e4
wxianxin <wxianxinreg@gmail.com>
2020-07-29 23:58:07 -0400

    update

--- vm.sh
+++ vm.sh
@@ -4,19 +4,19 @@
 # sudo sh -c 'echo "0000:01:00.2" > /sys/bus/pci/devices/0000:01:00.2/driver/unbind'
 # sudo sh -c 'echo "0000:01:00.2" > /sys/bus/pci/drivers/vfio-pci/bind'
 
-bash /home/coupe/kvm/bind_vfio.sh
-bash /home/coupe/kvm/set_cpu_performance.sh
+#bash /home/coupe/kvm/bind_vfio.sh
+sudo bash /home/coupe/kvm/set_cpu_performance.sh
 
-mount -t hugetlbfs hugetlbfs /dev/hugepages
-sysctl vm.nr_hugepages=6144
+sudo mount -t hugetlbfs hugetlbfs /dev/hugepages
+sudo sysctl vm.nr_hugepages=6144
 
 # Standard locations from the Ubuntu `ovmf` package; last one is arbitrary:
 export VGAPT_FIRMWARE_BIN=/usr/share/OVMF/OVMF_CODE.fd
 export VGAPT_FIRMWARE_VARS=/usr/share/OVMF/OVMF_VARS.fd
 export VGAPT_FIRMWARE_VARS_TMP=/tmp/OVMF_VARS.fd.tmp
 
-cp -f $VGAPT_FIRMWARE_VARS $VGAPT_FIRMWARE_VARS_TMP &&
-chrt -r 1 taskset -c 4-15 qemu-system-x86_64 \
+sudo cp -f $VGAPT_FIRMWARE_VARS $VGAPT_FIRMWARE_VARS_TMP &&
+sudo chrt -r 1 taskset -c 4-15 qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly,file=$VGAPT_FIRMWARE_BIN \
   -drive if=pflash,format=raw,file=$VGAPT_FIRMWARE_VARS_TMP \
   -enable-kvm \
@@ -36,12 +36,13 @@ chrt -r 1 taskset -c 4-15 qemu-system-x86_64 \
   -device vfio-pci,host=01:00.3 \
   -drive file=/dev/nvme0n1p3,format=raw,if=virtio,cache=none,index=0 \
   -drive file=/dev/nvme1n1p4,format=raw,if=virtio,cache=none,index=1 \
+  -usb -device usb-host,hostbus=3,hostaddr=2 \
   -usb -device usb-host,hostbus=3,hostaddr=3 \
   -usb -device usb-host,hostbus=5,hostaddr=3 \
 ;
 
-bash /home/coupe/kvm/set_cpu_ondemand.sh
-bash /home/coupe/kvm/undo_bind_vfio.sh
+sudo bash /home/coupe/kvm/set_cpu_ondemand.sh
+sudo bash /home/coupe/kvm/undo_bind_vfio.sh
 
 # taskset 0xFFF0 qemu-system-x86_64 \
 # -cpu host,kvm=off,topoext=on,hv_relaxed,hv_vapic,hv_time,hv_vpindex,hv_synic,hv_vendor_id=1234567890ab,hv_spinlocks=0x1fff \
