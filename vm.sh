80bc1b35d9cdb2c7d95b2968ad1d51ff8abb1a18
wxianxin <wxianxinreg@gmail.com>
2021-02-13 12:19:34 -0500

    add bridged network with tap device using virtio driver

--- vm.sh
+++ vm.sh
@@ -7,10 +7,17 @@
 
 ########################################################################################
 # network bridge
+# a bridge is like a virtual switch
+# a tap device is like a virtual nic
+# virtio driver can leverage tap as nic for guest
 sudo ip link add br0 type bridge
-sudo ip link set br0 up
+sudo ip link set dev br0 up
 sudo ip link set dev enp4s0 master br0
 sudo ip link set enp4s0 up
+sudo ip tuntap add mode tap tap0
+sudo ip link set tap0 master br0
+sudo ip link set tap0 up
+sudo dhclient br0
 ########################################################################################
 
 
@@ -27,6 +34,7 @@ export VGAPT_FIRMWARE_VARS_TMP=/tmp/OVMF_VARS.fd.tmp
 
 sudo cp -f $VGAPT_FIRMWARE_VARS $VGAPT_FIRMWARE_VARS_TMP &&
 sudo chrt -r 1 taskset -c 2-7 qemu-system-x86_64 \
+  -D ./qemu.log \
   -drive if=pflash,format=raw,readonly,file=$VGAPT_FIRMWARE_BIN \
   -drive if=pflash,format=raw,file=$VGAPT_FIRMWARE_VARS_TMP \
   -enable-kvm \
@@ -40,10 +48,11 @@ sudo chrt -r 1 taskset -c 2-7 qemu-system-x86_64 \
   -rtc base=localtime \
   -boot menu=on \
   -drive file=/home/coupe/D/vm/kvm_win10.qcow2,format=qcow2,if=virtio,cache=none,index=0 \
+  -drive file=/dev/nvme0n1p6,format=raw,if=virtio,cache=none,index=2 \
   -drive file=/dev/nvme0n1p7,format=raw,if=virtio,cache=none,index=1 \
   -device vfio-pci,host=02:00.0 \
   -device vfio-pci,host=02:00.1 \
-  -net nic -net bridge,br=br0 \
+  -device virtio-net,netdev=network0 -netdev tap,id=network0,ifname=tap0,script=no,downscript=no \
   -usb -device usb-host,hostbus=1,hostaddr=6 \
   -usb -device usb-host,hostbus=1,hostaddr=7 \
   -usb -device usb-host,hostbus=1,hostaddr=10 \
@@ -65,8 +74,8 @@ sudo chrt -r 1 taskset -c 2-7 qemu-system-x86_64 \
 # -drive file=/home/coupe/D/vm/virtio-win-0.1.185.iso,media=cdrom \
 # -device vfio-pci,host=01:00.0,romfile=/home/coupe/D/vm/navi_10.rom \
 # -acpitable file=/home/coupe/kvm/SSDT1.dat \
-# -net nic,model=virtio \
+# -net nic -net bridge,br=br0 \
 
 ########################################################################################
 # hv_vendor_id is used for Nvidia Error 43 prevention
-# !!! If created vm is using virtio, DO NOT qemu-system-x86_64 start without drive option "if=virtio", you will get BSOD
+# !!! If vm created using virtio, DO NOT qemu-system-x86_64 start without drive option "if=virtio", otherwise BSOD
