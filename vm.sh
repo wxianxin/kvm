edd19c7332a3fe9e7f81773b1c381875ad27b3ef
wxianxin <wxianxinreg@gmail.com>
2023-06-16 11:53:49 -0400

    update

--- vm.sh
+++ vm.sh
@@ -53,18 +53,20 @@ fi
 
 ########################################################################################
 # mount the storage. NOTE: this has to be after the network bridge setup.
-sudo mount /dev/nvme0n1p9 /home/coupe/D
+sudo mount /dev/nvme0n1p6 /home/$LOGNAME/D
+bash /home/$LOGNAME/config/fc.sh
 ########################################################################################
 # rebind GPU
 if [ "$rebind_GPU" == "yes" ]; then
     echo "rebind_GPU: $rebind_GPU"
-    sudo bash /home/coupe/kvm/bind_vfio.sh
+    sudo bash /home/$LOGNAME/kvm/bind_vfio.sh
+    sleep 5
 fi
 ########################################################################################
 # set AMD CPU to performance mode
 if [ "$amd_cpu_performance" == "yes" ]; then
     echo "amd_cpu_performance: $amd_cpu_performance"
-    sudo bash /home/coupe/kvm/set_cpu_performance.sh
+    sudo bash /home/$LOGNAME/kvm/set_cpu_performance.sh
 fi
 
 ########################################################################################
@@ -84,16 +86,16 @@ export VGAPT_FIRMWARE_VARS_TMP=/tmp/OVMF_VARS.fd.tmp
 
 sudo cp -f $VGAPT_FIRMWARE_VARS $VGAPT_FIRMWARE_VARS_TMP &&
 # sudo taskset 0xFFF0 qemu-system-x86_64 \
-# sudo chrt -r 1 taskset -c 4-15 /home/coupe/qemu-6.1.0/build/qemu-system-x86_64 \
+# sudo chrt -r 1 taskset -c 4-15 /home/$LOGNAME/qemu-6.1.0/build/qemu-system-x86_64 \
 # sudo chrt -r 1 taskset -c 0-11 qemu-system-x86_64 \
-sudo systemd-run --slice=steven_qemu.slice  --unit=steven_qemu --property="AllowedCPUs=0-15" qemu-system-x86_64 \
+sudo systemd-run --slice=steven_qemu.slice  --unit=steven_qemu --property="AllowedCPUs=0-13" qemu-system-x86_64 \
   --name stevenqemu,debug-threads=on \
   --pidfile /run/steven_qemu.pid \
   --drive if=pflash,format=raw,readonly=on,file=$VGAPT_FIRMWARE_BIN \
   --drive if=pflash,format=raw,file=$VGAPT_FIRMWARE_VARS_TMP \
   --enable-kvm \
   --machine q35,accel=kvm,mem-merge=off \
-  --cpu host,kvm=off,topoext=on,host-cache-info=on,hv_relaxed,hv_vapic,hv_time,hv_vpindex,hv_synic,hv_stimer,hv_frequencies,hv_reset,hv_vendor_id=eeag,hv_spinlocks=0x1fff \
+  --cpu host,kvm=off,topoext=on,host-cache-info=on,hv_relaxed,hv_vapic,hv_time,hv_vpindex,hv_synic,hv_stimer,hv_frequencies,hv_reset,hv_vendor_id=1002,hv_spinlocks=0x1fff \
   --smp 12,sockets=1,cores=6,threads=2 \
   --m 16384 \
   --mem-prealloc \
@@ -105,12 +107,11 @@ sudo systemd-run --slice=steven_qemu.slice  --unit=steven_qemu --property="Allow
   --rtc base=localtime \
   --boot menu=on \
   --object iothread,id=io0 \
-  --blockdev file,node-name=f0,filename=/home/coupe/D/vm/win11_0.qcow2 \
+  --blockdev file,node-name=f0,filename=/home/$LOGNAME/D/vm/win10.qcow2 \
   --blockdev qcow2,node-name=q0,file=f0 \
   --device virtio-blk-pci,drive=q0,iothread=io0 \
   --blockdev host_device,node-name=q1,filename=/dev/sda2 \
   --device virtio-blk-pci,drive=q1,iothread=io0 \
-  --drive file=/home/coupe/D/vm/virtio-win-0.1.229.iso,media=cdrom \
   --device pcie-root-port,id=abcd,chassis=1 \
   --device vfio-pci,host=03:00.0,bus=abcd,addr=00.0,multifunction=on \
   --device vfio-pci,host=03:00.1,bus=abcd,addr=00.1 \
@@ -134,24 +135,24 @@ sudo systemd-run --slice=steven_qemu.slice  --unit=steven_qemu --property="Allow
 # undo rebind GPU
 if [ "$reverse_rebind_GPU" == "yes" ]; then
     echo "reverse_rebind_GPU: $reverse_rebind_GPU"
-    sudo bash /home/coupe/kvm/bind_vfio_undo.sh
+    sudo bash /home/$LOGNAME/kvm/bind_vfio_undo.sh
 fi
 
 ########################################################################################
 # set AMD CPU back to ondemand mode
 if [ "$amd_cpu_performance" == "yes" ]; then
     echo "amd_cpu_performance: $amd_cpu_performance"
-    sudo bash /home/coupe/kvm/set_cpu_ondemand.sh
+    sudo bash /home/$LOGNAME/kvm/set_cpu_ondemand.sh
 fi
 ########################################################################################
 
 # --vga none \
-# --device vfio-pci,host=01:00.0,romfile=/home/coupe/D/vm/TU106.rom \
-# --drive if=none,id=disk0,cache=none,aio=threads,format=qcow2,file=/home/coupe/vm/win11.qcow2 \
+# --device vfio-pci,host=01:00.0,romfile=/home/$LOGNAME/D/vm/TU106.rom \
+# --drive if=none,id=disk0,cache=none,aio=threads,format=qcow2,file=/home/$LOGNAME/vm/win11.qcow2 \
 # --drive if=none,id=disk1,cache=none,aio=threads,format=raw,file=/dev/nvme0n1p5 \
-# --drive file=/home/coupe/D/vm/Win11_English_x64v1.iso,media=cdrom \
-# --drive file=/home/coupe/D/vm/virtio-win-0.1.215.iso,media=cdrom \
-# --acpitable file=/home/coupe/kvm/SSDT1.dat \
+# --drive file=/home/$LOGNAME/D/vm/Win11_English_x64v1.iso,media=cdrom \
+# --drive file=/home/$LOGNAME/D/vm/virtio-win-0.1.215.iso,media=cdrom \
+# --acpitable file=/home/$LOGNAME/kvm/SSDT1.dat \
 # --usb -device usb-host,hostbus=1,hostaddr=7 \ # legacy USB passthrough(usb1.1/2.0)
 
 ########################################################################################
@@ -160,4 +161,4 @@ fi
 # chrt: -r robin round scheduler
 # USB: ehci(usb2.0) xchi(usb3.0) controller
 # About audio device (also QEMU USB emulator): such implementation requires very tight timing on the clock, so the performance is usually not satisfactory at first and it needs extensive tweaking.
-# Error about init pa when sudo qemu: fix: sudo cp /home/coupe/.config/pulse/cookie /root/.config/pulse/cookie
+# Error about init pa when sudo qemu: fix: sudo cp /home/$LOGNAME/.config/pulse/cookie /root/.config/pulse/cookie
