0d1b8e819393d5ab1be166ecba43f288b209998a
wxianxin <wxianxinreg@gmail.com>
2025-04-10 07:15:35 -0400

    update

--- scripts/set_cpu_lp.sh
+++ scripts/set_cpu_lp.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-for c in {0..9} {16..25}; do
-  echo 1800000 | sudo tee /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq
-  maxf=$(cat /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq)
-  echo "cpu${c}: $((maxf/1000)) MHz"
-done
+
+set_cpu_lp() {
+  for c in {0..31}; do
+    echo 1800000 | sudo tee /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq
+    maxf=$(cat /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq)
+    echo "cpu${c}: $((maxf/1000)) MHz"
+  done
+}
+
+set_cpu_hp() {
+  for c in {0..31}; do
+    echo 5752000 | sudo tee /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq
+    # echo 1800000 | sudo tee /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq
+    maxf=$(cat /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq)
+    echo "cpu${c}: $((maxf/1000)) MHz"
+  done
+}
+
+set_cpu_hp() {
+  for c in {0..31}; do
+    default_max=$(cat /sys/devices/system/cpu/cpu${c}/cpufreq/cpuinfo_max_freq)
+    echo "$default_max" | sudo tee /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq
+    current_max=$(cat /sys/devices/system/cpu/cpu${c}/cpufreq/scaling_max_freq)
+    echo "cpu${c}: $((current_max/1000)) MHz"
+  done
+}
+
+# set_cpu_lp
+set_cpu_hp
